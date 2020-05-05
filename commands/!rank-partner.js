ed6723c7333304c4fe18b93de339cd0f23f9ab5e
danivanbussel <64401680+danivanbussel@users.noreply.github.com>
2020-05-05 21:01:58 +0200

    f
    f

--- commands/!rank-partner.js
+++ commands/!rank-partner.js
@@ -6,7 +6,7 @@ module.exports.run = async(bot, message, args) => {
                     var botEmbed = new discord.MessageEmbed()
                     .setTitle("**__[💼] Partner__**")
                     .setColor("#504fb6")
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/695319903523045457/704031383676518471/RaceTopia_2.png`)
+                    .setThumbnail(`https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/briefcase_1f4bc.png`)
                     .setDescription("Beschrijving")
                     .setFooter('Gemaakt door: Dani van Bussel', 'https://lh3.googleusercontent.com/-JubvaieWRSc/XoeJgjnRh8I/AAAAAAAAF_E/0zQNDI2_1AEYjdrYXBegO_IAcd_G-LE2QCEwYBhgL/w140-h139-p/20180712_091048.jpg');
     
