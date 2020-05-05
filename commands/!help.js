448f369c91c07abe5fa314d5644835469816ccbf
danivanbussel <64401680+danivanbussel@users.noreply.github.com>
2020-05-05 14:00:41 +0200

    Update !help.js

--- commands/!help.js
+++ commands/!help.js
@@ -14,6 +14,7 @@ module.exports.run = async(bot, message, args) => {
                     .addField("**__[🔨] Mod__**", "`!kick <naam> <reden>` - Om iemand van de server te kicken!\n`!tempban <naam> <reden>` - Om iemand voor een tijdje te bannen!")
                     .addField("**__[👔] Staff__**", "`!tempmute <naam> <reden>` - Om iemand een tijdje te muten!\n`!mute <naam> <reden>` - Om iemand te muten!\n`!warn - <naam> <reden>` - Om iemand te waarschuwen!")
                     .addField("**__[👱‍♂️] Player__**", "`!help` - Voor alle bot command's! \n `!ip` - Voor alle MineCraft Server informatie! \n `!idee` - Om jou server ideën op te sturen! \n `!rank` - Voor alle rank informatie! \n `!regels` - Om alle server regels te lezen!")
+                    .addField("**__[🎫] service__**", "`-close <reden>` - Om een ticket te sluiten")
                     .setFooter('Gemaakt door: Dani van Bussel', 'https://lh3.googleusercontent.com/-JubvaieWRSc/XoeJgjnRh8I/AAAAAAAAF_E/0zQNDI2_1AEYjdrYXBegO_IAcd_G-LE2QCEwYBhgL/w140-h139-p/20180712_091048.jpg');
 
             return message.channel.send(botEmbed);
